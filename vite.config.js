b5faab0e820afee2e4c48592394bc6439233f272
yousefIbrahem-1 <youssefibrahem414@gmail.com>
2025-11-26 00:45:16 +0200

    Update GitHub Pages settings: homepage and Vite base for portfolio

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/temp/",
+  base: "/portfolio/",
   plugins: [
     react({
       babel: {
